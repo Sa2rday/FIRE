308dbaa3ca71b1cbcebd34ddc883f847c8c15d5d
Бяков Александр Александрович <a.a.byakov@academy.local>
2021-05-04 17:18:16 +0400

    First_Commit_B_Alexander

---
+++ FIVeff/Assets/Resources/Recources.Load.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class RecourcesLoad : MonoBehaviour
+{
+    void Start()
+    {
+        var model = Resources.Load<TextAsset>("glock.fbx");
+        var textureNor_M = Resources.Load<Texture2D>("Glock_normal_map/Main_metall_second_normal_map4k");
+        var textureSpec_M = Resources.Load<Texture2D>("Glock_normal_map/MetalSpottyDiscoloration001_NRM_4K_SPECULAR"); 
+    }
+}
