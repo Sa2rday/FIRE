365a9bf0e2568262844f85ea1e89cd6802e373a3
Тюфтина Дарья Александровна <d.a.tyuftina@academy.local>
2021-05-04 17:52:42 +0400

    Next try

---
+++ fire menu/Assets/огонь/Assets/3D materials/3D models/buttons/buttonMenu.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.SceneManager;
+
+public class buttonMenu : MonoBehaviour
+{
+    public GameObject Angar;        //обозначаю объекты между которыми буду переключаться
+    public GameObject Menu;
+    public GameObject Options;
+    public GameObject Play;
+    public GameObject Rifles;
+    public GameObject Automatic;
+    public GameObject Pistols;
+
+    public void AngarButton()       //public void создаёт функция, дальше её название
+    {
+        Angar.SetActive(true);      //включаю объект, SetActive() может выключать и включать объекты на сцене
+        Menu.SetActive(false);      //при true объект включён при false выключен
+    }
+    public void ExitButton()
+    {
+        Application.Quit();         //Application.Quit() выключает приложение
+    }
+    public void OptionsButton()
+    {
+        Options.SetActive(true);
+        Menu.SetActive(false);
+    }
+    public void AngarMenuButton()
+    {
+        Angar.SetActive(false);
+        Menu.SetActive(true);
+    }
+    public void OPtionsMenuButton()
+    {
+        Options.SetActive(false);
+        Menu.SetActive(true);
+    }
+    public void PlayButton() 
+    {
+        //SceneManager.LoadScene();           //SceneManager.LoadScene(name) загружает сцену 
+    }
+    public void RiflesButton()
+    {
+        Rifles.SetActive(true);
+        Angar.SetActive(false);
+    }
+    public void RiflesAngarButton()
+    {
+        Angar.SetActive(false);
+        Menu.SetActive(true);
+    }
+    public void AutomaticButton()
+    {
+        Automatic.SetActive(true);
+        Angar.SetActive(false);
+    }
+    public void AutomaticAngarButton()
+    {
+        Automatic.SetActive(false);
+        Angar.SetActive(true);
+    }
+    public void PistolsButton()
+    {
+        Pistols.SetActive(true);
+        Angar.SetActive(false);
+    }
+    public void PistolsAngarButton()
+    {
+        Pistols.SetActive(false);
+        Angar.SetActive(true);
+    }
+}
